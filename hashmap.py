4060582e5a634ee7345d0e10cc531d1122f893a5
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-04 13:16:24 +0500

    create hashmap hinter class to manage handler hashMap

---
+++ hashmap.py
@@ -0,0 +1,13 @@
+class HashMap:
+    """Base hash map class."""
+    def get(self, key: str) -> str:
+        pass
+
+    def containsKey(self, key: str) -> bool:
+        pass
+
+    def put(self, key: str, value: str) -> None:
+        pass
+
+    def remove(self, key: str) -> None:
+        pass
