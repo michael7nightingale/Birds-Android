5ee1da783385d7034f000e03192a265dbefa4153
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-04 23:57:13 +0500

    create base repository class

--- base.py
+++ base.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from typing import Type
+from pony.orm import Database, db_session
+from pony.orm.core import Entity, EntityMeta
 
 from hashmap import HashMap
 
@@ -18,3 +21,24 @@ class BaseScreen(ABC):
     def on_start(cls, hashMap: HashMap, _files=None, _data=None) -> HashMap:
         """Start scree event handler."""
         pass
+
+
+class BaseRepository(ABC):
+    """Base repository class."""
+    entity: EntityMeta
+
+    @classmethod
+    @abstractmethod
+    def create(cls, bird_id: str):
+        pass
+
+    @classmethod
+    @db_session
+    def all(cls):
+        return cls.entity.select()
+
+    @classmethod
+    @db_session
+    def get(cls, id_: str):
+        obj = cls.entity.get(id=id_)
+        return obj
