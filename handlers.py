5a7bc04ca74c73b2e60c2a0872c962d46c382cf6
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-04 13:17:06 +0500

    create handler endpoints to connect to .ui file handlers

---
+++ handlers.py
@@ -0,0 +1,41 @@
+from menu import Menu
+from birds import Birds
+from detail import Detail
+from add import Add
+from hashmap import HashMap
+
+
+# main menu
+def menu_on_start(hashMap: HashMap, _files=None, _data=None) -> HashMap:
+    return Menu.on_start(hashMap, _files, _data)
+
+
+def menu_on_input(hashMap: HashMap, _files=None, _data=None) -> HashMap:
+    return Menu.on_input(hashMap, _files, _data)
+
+
+# birds list
+def birds_list_on_start(hashMap: HashMap, _files=None, _data=None) -> HashMap:
+    return Birds.on_start(hashMap, _files, _data)
+
+
+def birds_list_on_input(hashMap: HashMap, _files=None, _data=None) -> HashMap:
+    return Birds.on_input(hashMap, _files, _data)
+
+
+# add bird
+def add_bird_on_start(hashMap: HashMap, _files=None, _data=None) -> HashMap:
+    return Add.on_start(hashMap, _files, _data)
+
+
+def add_bird_on_input(hashMap: HashMap, _files=None, _data=None) -> HashMap:
+    return Add.on_input(hashMap, _files, _data)
+
+
+# bird detail
+def bird_detail_on_start(hashMap: HashMap, _files=None, _data=None) -> HashMap:
+    return Detail.on_start(hashMap, _files, _data)
+
+
+def bird_detail_on_input(hashMap: HashMap, _files=None, _data=None) -> HashMap:
+    return Detail.on_input(hashMap, _files, _data)
